65353e0d2e2cd6785d72f103d01b991ffeb18a04
Taylor Plewe <tplewe@outlook.com>
2026-01-07 12:17:32 -0700

    initial commit; add games list

--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,15 @@
 <script lang="ts">
   import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
   import Counter from './lib/Counter.svelte'
 </script>
 
 <main>
   <div>
     <a href="https://vite.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
+      vite
     </a>
     <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
+      svelte
     </a>
   </div>
   <h1>Vite + Svelte</h1>
