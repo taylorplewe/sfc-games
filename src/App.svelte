0f58c6abf16739b615194a87af6258e018954cee
Taylor Plewe <tplewe@outlook.com>
2026-01-08 13:24:55 -0700

    clean up portrait vs. landscape styling

--- src/App.svelte
+++ src/App.svelte
@@ -1,17 +1,31 @@
 <script lang="ts">
 import GameData from "./GameData";
 import gamesRaw from "./games.json";
-
 const games: GamesData[] = gamesRaw;
 
 const getGameTitle = game => game.titleEn || game.titleJpRomaji;
+let gameBoxArtEls: ImgElement[] = [];
+const getIsBoxArtPortrait = (i): boolean => {
+  console.log(i);
+  // console.log(gameBoxArtEls);
+  // console.log(JSON.stringify(gameBoxArtEls[i]));
+  console.log(gameBoxArtEls[i]?.clientWidth);
+  console.log(gameBoxArtEls[i]?.clientHeight);
+  return gameBoxArtEls[i]
+    ? gameBoxArtEls[i].clientHeight > gameBoxArtEls[i].clientWidth
+    : true;
+}
 </script>
 
+<hgroup>
+  <h1>Taylor's list of notable Super Famicom games</h1>
+  <p>Never released in the USA</p>
+</hgroup>
 <main>
   <div class="games">
-    {#each games as game}
+    {#each games as game, i}
       <div class="game">
-        <img class="game-boxart" src={game.boxArtUrl}>
+        <img class={["game-boxart", game.isBoxArtPortrait && "portrait"]} src={game.boxArtUrl}>
         <h3 class="title">{getGameTitle(game)}</h3>
         <p>{game.titleJp}</p>
         {#if game.titleEn}
@@ -28,6 +42,11 @@ main {
   max-width: 90%;
 }
 
+hgroup {
+  width: max-content;
+  margin: 64px auto 128px auto;
+}
+
 .title {
   font-size: 18pt;
   font-weight: 600;
@@ -37,11 +56,13 @@ main {
 .games {
   display: grid;
   grid-template-columns: repeat(auto-fill, minmax(400px, auto));
-  gap: 32px;
+  gap: 48px;
 }
 .game {
   padding: 8px;
   border-radius: 8px;
+  margin: auto 0;
+  height: max-content;
   text-align: center;
   cursor: pointer;
   transition: scale 0.5s ease;
@@ -52,6 +73,8 @@ main {
 .game-boxart {
   object-fit: contain;
   width: 100%;
-  aspect-ratio: 1;
+  &.portrait {
+    aspect-ratio: 1;
+  }
 }
 </style>
--- src/GameData.ts
+++ src/GameData.ts
@@ -1,19 +1,26 @@
 export default interface GameData {
+  // textual info
   titleEn: string,
   titleJp: string,
   titleJpRomaji: string,
-  genre: string,
-  description: string,
   developer: string,
   publisher: string,
+  genre: string,
+  description: string,
+  funFacts: string[],
+  howLongToBeatHrs: number,
+
+  // visuals
   gameplayStillUrl: string,
   gameplayGifUrl: string,
   boxArtUrl: string,
+  isBoxArtPortrait: boolean,
+
+  // technical specs
   releaseDate: string,
   sizeRomInMebibits: number,
   sizeSramInKibibits: number,
   isHiRom: boolean,
   isFastRom: boolean,
   specialChips: string[],
-  funFacts: string[],
 }
